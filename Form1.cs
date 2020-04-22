e6a4ff02088ec33abc589f18d940b032a0b2b7df
Roman666shiyanov <60029536+Roman666shiyanov@users.noreply.github.com>
2020-04-22 15:21:20 +0300

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -193,8 +193,189 @@ namespace WindowsFormsApp7
             
             textBox4.Text = textBox3.Text.Replace("чьк", "чк");
         }
-        
-            private void button17_Click(object sender, EventArgs e)
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+                                        //Конвертер
+
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+
+            double i = int.Parse(Text_Box7.Text);
+
+            // перевод из сантиметра 
+
+            if (Combo_Box1.SelectedItem == "Сантиметр" && Combo_Box2.SelectedItem == "Дюйм")// Сантиметр - Дюйм
+            {
+                double conver = i * 0.3937;
+                Label10.Text = "Конвертированная сумма: " + conver;
+            }
+            if (Combo_Box1.SelectedItem == "Сантиметр" && Combo_Box2.SelectedItem == "Фут")// Сантиметр - Фут
+            {
+                double conver = i * 0.0328;
+                Label10.Text = "Конвертированная сумма: " + conver;
+                
+            }
+            if (Combo_Box1.SelectedItem == "Сантиметр" && Combo_Box2.SelectedItem == "Пье")//Сантиметр - Пье
+            {
+                double conver = i * 0.030788;
+                Label10.Text = "Конвертированная сумма: " + conver;
+
+            }
+            if (Combo_Box1.SelectedItem == "Сантиметр" && Combo_Box2.SelectedItem == "Пьеда")//Сантиметр - Пьеда
+            {
+                double conver = i * 0.033602;
+                Label10.Text = "Конвертированная сумма: " + conver;
+               
+                
+                // Перевод из Дюйма
+
+
+            }
+            double c = int.Parse(Text_Box7.Text);
+            if (Combo_Box1.SelectedItem == "Дюйм" && Combo_Box2.SelectedItem == "Сантиметр") //Дюйм-Сантиметр
+            {
+                double conver = i * 2.54;
+                Label10.Text = "Конвертированная сумма: " + conver;
+            }
+            if (Combo_Box1.SelectedItem == "Дюйм" && Combo_Box2.SelectedItem == "Фут") //Дюйм- Фут
+            {
+                double conver = i * 0.0833;
+                Label10.Text = "Конвертированная сумма: " + conver;
+
+            }
+            if (Combo_Box1.SelectedItem == "Дюйм" && Combo_Box2.SelectedItem == "Пье") // Дюйм - Пье
+            {
+                double conver = i * 0.09117;
+                Label10.Text = "Конвертированная сумма: " + conver;
+
+            }
+            if (Combo_Box1.SelectedItem == "Дюйм" && Combo_Box2.SelectedItem == "Пьеда") //Дюйм - Пьеда
+            {
+                double conver = i * 0.2354;
+                Label10.Text = "Конвертированная сумма: " + conver;
+
+            }
+
+
+            // перевод из Фута
+
+
+            double r = int.Parse(Text_Box7.Text);
+            if (Combo_Box1.SelectedItem == "Фут" && Combo_Box2.SelectedItem == "Сантиметр") //фут -Сантиметр
+            {
+                double conver = i * 30.48;
+                Label10.Text = "Конвертированная сумма: " + conver;
+            }
+            if (Combo_Box1.SelectedItem == "Фут" && Combo_Box2.SelectedItem == "Дюйм") // Фут - Дюйм
+            {
+                double conver = i * 12;
+                Label10.Text = "Конвертированная сумма: " + conver;
+
+            }
+            if (Combo_Box1.SelectedItem == "Дюйм" && Combo_Box2.SelectedItem == "Пье") // Фут - Пье
+            {
+                double conver = i * 0.938424;
+                Label10.Text = "Конвертированная сумма: " + conver;
+
+            }
+            if (Combo_Box1.SelectedItem == "Дюйм" && Combo_Box2.SelectedItem == "Пьеда") // Фут -Пьеда
+            {
+                double conver = i * 0.2564;
+                Label10.Text = "Конвертированная сумма: " + conver;
+
+            }
+
+            //перевод из Пье
+
+
+            double l = int.Parse(Text_Box7.Text);
+            if (Combo_Box1.SelectedItem == "Пье" && Combo_Box2.SelectedItem == "Сантиметр") //Пье -Сантиметр
+            {
+                double conver = i * 32.48;
+                Label10.Text = "Конвертированная сумма: " + conver;
+            }
+            if (Combo_Box1.SelectedItem == "Пье" && Combo_Box2.SelectedItem == "Дюйм") // Пье - Дюйм
+            {
+                double conver = i * 10.968504;
+                Label10.Text = "Конвертированная сумма: " + conver;
+
+            }
+            if (Combo_Box1.SelectedItem == "Пье" && Combo_Box2.SelectedItem == "Фут") // Пье - фут
+            {
+                double conver = i * 1.0940;
+                Label10.Text = "Конвертированная сумма: " + conver;
+
+            }
+            if (Combo_Box1.SelectedItem == "Пье" && Combo_Box2.SelectedItem == "Пьеда") // Пье - Пьеда
+            {
+                double conver = i * 0.76602;
+                Label10.Text = "Конвертированная сумма: " + conver;
+
+            }
+
+            //перевод из Пьеды
+
+            double n = int.Parse(Text_Box7.Text);
+            if (Combo_Box1.SelectedItem == "Пьеда" && Combo_Box2.SelectedItem == "Сантиметр") //Пьеда -Сантиметр
+            {
+                double conver = i * 29.76;
+                Label10.Text = "Конвертированная сумма: " + conver;
+            }
+            if (Combo_Box1.SelectedItem == "Пьеда" && Combo_Box2.SelectedItem == "Дюйм") // Пьеда - Дюйм
+            {
+                double conver = i * 1.546574;
+                Label10.Text = "Конвертированная сумма: " + conver;
+
+            }
+            if (Combo_Box1.SelectedItem == "Пьеда" && Combo_Box2.SelectedItem == "Пье") // Пьеда - Пье
+            {
+                double conver = i * 1.09;
+                Label10.Text = "Конвертированная сумма: " + conver;
+
+            }
+            if (Combo_Box1.SelectedItem == "Пьеда" && Combo_Box2.SelectedItem == "Дюйм") // Пьеда - Фут
+            {
+                double conver = i * 1.03 ;
+                Label10.Text = "Конвертированная сумма: " + conver;
+
+            }
+            {
+
+            }
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button17_Click(object sender, EventArgs e)
         {
             textBox1.Text = textBox1.Text + ",";
         }
